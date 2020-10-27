2c4b759a894f0dd6e5a37a70bf494532fbaa5a44
Dharmendra Kumar Awasthi <awadha01@CSGICORP.COM>
2020-10-27 09:47:45 +0530

    Initialized Master

---
+++ src/test/java/com/techendear/app/TddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techendear.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
